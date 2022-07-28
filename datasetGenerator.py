4e65a37ac380594fedca8acb4afd194e7e8292f4
AgapovDP <90335342+AgapovDP@users.noreply.github.com>
2022-07-28 13:45:03 +0300

    Update datasetGenerator.py

--- datasetGenerator.py
+++ datasetGenerator.py
@@ -20,7 +20,7 @@ def matrixLAA(theta = 0, P = 1, randomMod = True):
     M11 = np.cos(theta)**2 + P*np.sin(theta)**2
     M12 = M21 = (1-P)*np.cos(theta)*np.sin(theta)
     M22 = np.sin(theta)**2 + P*np.cos(theta)**2
-    return [[M11,M12],[M21,M22]]
+    return np.array([[M11,M12],[M21,M22]])
 
 #Jones matrix of Linear Phase Anisotropy (LPA).
 def matrixLPA(alpha = 0, delta = 0, randomMod = True):
@@ -30,7 +30,7 @@ def matrixLPA(alpha = 0, delta = 0, randomMod = True):
     M11 = np.cos(alpha)**2 + np.exp(-1j*delta)*np.sin(alpha)**2
     M12 = M21 = (1-np.exp(-1j*delta))*np.cos(alpha)*np.sin(alpha)
     M22 = np.sin(alpha)**2 + np.exp(-1j*delta)*np.cos(alpha)**2
-    return [[M11,M12],[M21,M22]]
+    return np.array([[M11,M12],[M21,M22]])
 
 #Jones matrix of Circular Amplitude Anisotropy (CAA)
 def matrixCAA(R = 0,randomMod = True):
@@ -38,7 +38,7 @@ def matrixCAA(R = 0,randomMod = True):
     M11 = M22 = 1.
     M12 = -1j*R
     M21 = 1j*R
-    return [[M11,M12],[M21,M22]]
+    return np.array([[M11,M12],[M21,M22]])
 
 #Jones matrix of Circular Phase Anisotropy (CPA)
 def matrixCPA(phi = 0,randomMod = True):
@@ -46,14 +46,16 @@ def matrixCPA(phi = 0,randomMod = True):
     M11 =  M22 = np.cos(phi)
     M12 = np.sin(phi)
     M21 = -np.sin(phi)
-    return [[M11,M12],[M21,M22]]
+    return np.array([[M11,M12],[M21,M22]])
 
+#function to calculete final Jones matrix
 def calculationFinalMatrix(objectVector):
-    matrix = [[1,0],[0,1]]
+    matrix = np.array([[1,0],[0,1]])
     if objectVector[0] == 1: matrix = matrix.dot(matrixLAA())
     if objectVector[1] == 1: matrix = matrix.dot(matrixLPA())
     if objectVector[2] == 1: matrix = matrix.dot(matrixCAA())
     if objectVector[3] == 1: matrix = matrix.dot(matrixCPA())
+    return matrix
 
 def colcilationCorrFunctions():
     return 0
