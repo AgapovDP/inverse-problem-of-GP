0f9930a64383eaa93bee8a0b85f3d30638800ddc
AgapovDP <90335342+AgapovDP@users.noreply.github.com>
2022-08-15 10:50:29 +0300

    Добавил первую версию сети.

--- GPNN.py
+++ GPNN.py
@@ -7,10 +7,11 @@ Created on Tue Jul 26 23:59:16 2022
 
 
 import torch.nn as nn
-
+from dataConvertor import dataConventor,dataBatching
+import numpy as np
 
 standartSeq = nn.Sequential(
-     nn.Linear(8, 256),
+     nn.Linear(3, 256),
      nn.Unflatten(1, (1,16, 16)),
      nn.ReLU(),
      nn.Conv2d(1, 20, 2, padding=2),
@@ -18,7 +19,7 @@ standartSeq = nn.Sequential(
      nn.Conv2d(20, 50, 2, padding=2),
      nn.Flatten(),
      nn.ReLU(),
-     nn.Linear(24200, 8)
+     nn.Linear(24200, 7)
 
 )
 
@@ -37,46 +38,55 @@ class GPNN(nn.Module):
 import torch
 import torch.optim as optim
 
-def trainGPNN(trainloader,testloader):
+model = GPNN()
+model = model.float()
+
+def trainGPNN(model, trainloader,testloader):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using device:', device)
-    model = GPNN().to(device)
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
-    num_epochs = 2
+    num_epochs = 20
     loss_hist = [] # for plotting
     val_arr_test = []
     val_arr_train = []
     for epoch in range(num_epochs):
         hist_loss = 0
-        for _, batch in enumerate(trainloader, 0): # get batch
-            # parse batch 
-            imgs, labels = batch
-            imgs, labels = imgs.to(device), labels.to(device)
-            # sets the gradients of all optimized tensors to zero.
-            optimizer.zero_grad() 
-            # get outputs
-            Y_pred = model(imgs) 
-            # calculate loss
-            loss = criterion(Y_pred, labels)
-            # calculate gradients
-            loss.backward() 
-            # performs a single optimization step (parameter update)
-        optimizer.step()
-        hist_loss += loss.item()
+        numBatch = 0
+        while numBatch < len(trainloader):
+            value, labels = trainloader[numBatch]
+            numBatch = numBatch + 1
+            optimizer.zero_grad()
+            Y_pred = model(value.float())
+            loss = criterion(Y_pred, labels.float())
+            loss.backward()    
+            optimizer.step()
+            hist_loss += loss.item()
         loss_hist.append(hist_loss /len(trainloader))
-        val_arr_train.append(validate(model,trainloader,device))
-        val_arr_test.append(validate(model,testloader,device))
+        #val_arr_train.append(validate(model,trainloader,device))
+        #val_arr_test.append(validate(model,testloader,device))
         print(f"Epoch={epoch} loss={loss_hist[epoch]:.4f}")
+    return loss_hist
 
-def validate(model,testloader,device):
+def validate(model,data):
     correct = 0
     total = 0
+    numBatch = 0
     with torch.no_grad():
-        for images, labels in testloader:
-            outputs = model(images.to(device))
+        while numBatch < len(data):
+            value, labels = data[numBatch]
+            labels = labels.float()
+            numBatch = numBatch + 1
+            outputs = model(value.float())
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
-            correct += (predicted == labels.to(device)).sum().item()
+            correct += (predicted == labels.float()).sum().item()
 
     return correct / total 
+
+
+testData = np.load("testLAA_test.npy",allow_pickle=True)
+trainData = np.load("testLAA_train.npy",allow_pickle=True)
+trainData = dataBatching(dataConventor(trainData),100)
+testData = dataBatching(dataConventor(testData),100)
+trainGPNN(model,trainData,testData)
--- dataConvertor.py
+++ dataConvertor.py
@@ -6,16 +6,34 @@ Created on Fri Aug  5 14:20:35 2022
 @author: dmitryagapov
 """
 
-from torch import from_numpy
+from torch import from_numpy, split, tensor
 import numpy as np
 
-test = np.load("testLAA.npy",allow_pickle=True)
+
 
 def dataConventor(data):
     inputData = []
     outputData = []
-    for i in test:
+    for i in data:
         inputData.append(i[0])
-        outputData.append(i[1])
-    return from_numpy(np.array(inputData)),from_numpy(np.array(outputData))
+        vector = [i[1][0,0].real,i[1][1,0].real,i[1][1,1].real,\
+                  i[1][0,0].imag,i[1][0,1].imag,i[1][1,0].imag,\
+                      i[1][1,1].imag]
+        outputData.append(vector)
+    return tensor(inputData),tensor(outputData)
+
+
+def dataBatching(data,batch_len=1):
+    inputData =  split(data[0],batch_len)
+    outputData = split(data[1],batch_len)
+    batchedData = []
+    for i in range(len(inputData)):
+        batchedData.append([inputData[i],outputData[i]])
+    return batchedData
+        
+test = np.load("testLAA.npy",allow_pickle=True)
+train = dataBatching(dataConventor(test),10)
+
+
+
 
