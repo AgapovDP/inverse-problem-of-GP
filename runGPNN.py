633dcae8bdae878094949a7a8e581fb769a3436f
AgapovDP <90335342+AgapovDP@users.noreply.github.com>
2022-08-17 19:06:00 +0300

    Батч сайз уменьшил

--- runGPNN.py
+++ runGPNN.py
@@ -10,17 +10,22 @@ import numpy as np
 import random 
 import GPNN
 from dataConvertor import dataConventor,dataBatching
-
+import matplotlib.pyplot as plt
 
 
 model = GPNN.GPNN()
 model = model.float()
 testData = np.load("datasets/datasetNoNoise_LAA_train.npy",allow_pickle=True)
 trainData = np.load("datasets/datasetNoNoise_LAA_test.npy",allow_pickle=True)
-trainData = dataBatching(dataConventor(trainData),500)
-testData = dataBatching(dataConventor(testData),500)
-GPNN.trainGPNN(model,trainData,testData,num_epochs = 40)
+trainData = dataBatching(dataConventor(trainData),150)
+testData = dataBatching(dataConventor(testData),150)
+loss, trainVal, testVal = GPNN.trainGPNN(model,trainData,testData,num_epochs = 10)
 
 
-
-
+# просто промежуточная функция для формирования графиков. Удалю попозже
+def somePlot(data):
+    localData = [[],[],[],[],[],[],[],[]]
+    for j in range(8):
+        for i in range(20):
+            localData[j].append(data[i][j])
+    return localData
