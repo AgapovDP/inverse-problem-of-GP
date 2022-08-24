bbf9ec639b20940c3245bed402db4658b5915b6c
AgapovDP <90335342+AgapovDP@users.noreply.github.com>
2022-08-24 13:57:47 +0300

    В названии теперь длина датасета

--- runDataGenerator.py
+++ runDataGenerator.py
@@ -9,23 +9,23 @@ import datasetGenerator
 
 
 #длина первой группы данных
-lenFirstGroup = 3000
+lenFirstGroup = 2400
 NOISE_1 = 0.0
 NOISE_2 = 0.01
 NOISE_3 = 0.05
 
 
-datasetGenerator.dataGeneratorRandomObject("datasets/datasetNoNoise_randomObject_train",lenFirstGroup,NOISE_1)
-datasetGenerator.dataGeneratorRandomObject("datasets/datasetNoNoise_randomObject_test",lenFirstGroup,NOISE_1)
-datasetGenerator.dataGeneratorLAA("datasets/datasetNoNoise_LAA_train",lenFirstGroup,NOISE_1)
-datasetGenerator.dataGeneratorLAA("datasets/datasetNoNoise_LAA_test",lenFirstGroup,NOISE_1)
+datasetGenerator.dataGeneratorRandomObject("datasets/datasetNoNoise_randomObject_train_"+str(lenFirstGroup),lenFirstGroup,NOISE_1)
+datasetGenerator.dataGeneratorRandomObject("datasets/datasetNoNoise_randomObject_test_"+str(lenFirstGroup),lenFirstGroup,NOISE_1)
+datasetGenerator.dataGeneratorLAA("datasets/datasetNoNoise_LAA_train_"+str(lenFirstGroup),lenFirstGroup,NOISE_1)
+datasetGenerator.dataGeneratorLAA("datasets/datasetNoNoise_LAA_test_"+str(lenFirstGroup),lenFirstGroup,NOISE_1)
 
-datasetGenerator.dataGeneratorRandomObject("datasets/datasetLowNoise_randomObject_train",lenFirstGroup,NOISE_2)
-datasetGenerator.dataGeneratorRandomObject("datasets/datasetLowNoise_randomObject_test",lenFirstGroup,NOISE_2)
-datasetGenerator.dataGeneratorLAA("datasets/datasetLowNoise_LAA_train",lenFirstGroup,NOISE_2)
-datasetGenerator.dataGeneratorLAA("datasets/datasetLowNoise_LAA_test",lenFirstGroup,NOISE_2)
+#datasetGenerator.dataGeneratorRandomObject("datasets/datasetLowNoise_randomObject_train_"+str(lenFirstGroup),lenFirstGroup,NOISE_2)
+#datasetGenerator.dataGeneratorRandomObject("datasets/datasetLowNoise_randomObject_test_"+str(lenFirstGroup),lenFirstGroup,NOISE_2)
+#datasetGenerator.dataGeneratorLAA("datasets/datasetLowNoise_LAA_train_"+str(lenFirstGroup),lenFirstGroup,NOISE_2)
+#datasetGenerator.dataGeneratorLAA("datasets/datasetLowNoise_LAA_test_"+str(lenFirstGroup),lenFirstGroup,NOISE_2)
 
-datasetGenerator.dataGeneratorRandomObject("datasets/datasetMediumNoise_randomObject_train",lenFirstGroup,NOISE_3)
-datasetGenerator.dataGeneratorRandomObject("datasets/datasetMediumNoise_randomObject_test",lenFirstGroup,NOISE_3)
-datasetGenerator.dataGeneratorLAA("datasets/datasetMediumNoise_LAA_train",lenFirstGroup,NOISE_3)
-datasetGenerator.dataGeneratorLAA("datasets/datasetMediumNoise_LAA_test",lenFirstGroup,NOISE_3)
+#datasetGenerator.dataGeneratorRandomObject("datasets/datasetMediumNoise_randomObject_train_"+str(lenFirstGroup),lenFirstGroup,NOISE_3)
+#datasetGenerator.dataGeneratorRandomObject("datasets/datasetMediumNoise_randomObject_test_"+str(lenFirstGroup),lenFirstGroup,NOISE_3)
+#datasetGenerator.dataGeneratorLAA("datasets/datasetMediumNoise_LAA_train_"+str(lenFirstGroup),lenFirstGroup,NOISE_3)
+#datasetGenerator.dataGeneratorLAA("datasets/datasetMediumNoise_LAA_test_"+str(lenFirstGroup),lenFirstGroup,NOISE_3)
