f379a9d7e5ee3853dc5deb2dba7ea130cde5e6cb
AgapovDP <dimaagapov@mail.ru>
2022-08-27 01:22:48 +0300

    fix

--- runDataGenerator.py
+++ runDataGenerator.py
@@ -9,7 +9,7 @@ import datasetGenerator
 
 
 #длина первой группы данных
-lenFirstGroup = 12000
+lenFirstGroup = 500000
 NOISE_1 = 0.0
 NOISE_2 = 0.01
 NOISE_3 = 0.05
@@ -19,9 +19,9 @@ datasetGenerator.dataGeneratorRandomObject("datasets/datasetNoNoise_randomObject
 datasetGenerator.dataGeneratorRandomObject("datasets/datasetNoNoise_randomObject_test_"+str(lenFirstGroup),lenFirstGroup,NOISE_1)
 #datasetGenerator.dataGeneratorLAA("datasets/datasetNoNoise_LAA_train_"+str(lenFirstGroup),lenFirstGroup,NOISE_1)
 #datasetGenerator.dataGeneratorLAA("datasets/datasetNoNoise_LAA_test_"+str(lenFirstGroup),lenFirstGroup,NOISE_1)
-
-#datasetGenerator.dataGeneratorRandomObject("datasets/datasetLowNoise_randomObject_train_"+str(lenFirstGroup),lenFirstGroup,NOISE_2)
-#datasetGenerator.dataGeneratorRandomObject("datasets/datasetLowNoise_randomObject_test_"+str(lenFirstGroup),lenFirstGroup,NOISE_2)
+print("Step 2")
+datasetGenerator.dataGeneratorRandomObject("datasets/datasetLowNoise_randomObject_train_"+str(lenFirstGroup),lenFirstGroup,NOISE_2)
+datasetGenerator.dataGeneratorRandomObject("datasets/datasetLowNoise_randomObject_test_"+str(lenFirstGroup),lenFirstGroup,NOISE_2)
 #datasetGenerator.dataGeneratorLAA("datasets/datasetLowNoise_LAA_train_"+str(lenFirstGroup),lenFirstGroup,NOISE_2)
 #datasetGenerator.dataGeneratorLAA("datasets/datasetLowNoise_LAA_test_"+str(lenFirstGroup),lenFirstGroup,NOISE_2)
 
